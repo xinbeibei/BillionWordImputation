c9c4e36f18da85778681d13f7e3968f442ab2dea
Roelof Groenewald <groenewa@usc.edu>
2017-04-25 15:53:29 -0700

    Update ngramTrain.py
    Added argparse to allow turning of collecting the vocabulary in order to save time when calculating various n value probabilities

--- ngramTrain.py
+++ ngramTrain.py
@@ -8,12 +8,19 @@ import math
 from collections import defaultdict
 import string
 import numpy as np
-
+import argparse
+parser = argparse.ArgumentParser()
 startTime = time.time()
+
+parser.add_argument("input", help="input file name")
+parser.add_argument("n", help="specify the value of N", type=int)
+parser.add_argument("-out", help="directory to save output", default='')
+parser.add_argument("-vocab", help="store vocabulary", action="store_true")
+args = parser.parse_args()
 
 phrases = {}	# this is the dictionary to store all the n-gram phrases
 vocabulary = [] # this is the full vocabulary of words
-N = int(sys.argv[2])
+N = args.n
 
 def getNgram(input_list, n):
 	return zip(*[input_list[i:] for i in range(n)])
@@ -22,29 +29,26 @@ def getNgram(input_list, n):
 	## Read the input files
 	## ----------------------------------------------
 
-try:
-	print("\n\n\tN-gram Training Algorithm\n\n\tLoading training data...")
+print("\n\n\tN-gram Training Algorithm\n\n\tLoading training data...")
 	
-	textFile = open(sys.argv[1])
-	
-	sentences = []
-	for line in textFile:
-		line = line.translate(None, string.punctuation).lower().replace('\n', '')
-		sentences.append(line)
-
+if args.vocab:
+	# get the vocabulary
+	textFile = open(args.input)
+	vocabulary = set(textFile.read().split())
 	textFile.close()
-except:
-	print ('\t'+'*'*56)
-	print ('\tError occurred during reading of training data... \n\tProgram terminated')
-	print ('\t'+'*'*56)
-	exit()
 
-endTime = time.time()
-print ("\tLoading training data complete\t\t%f seconds\n\n\tLearning started..." % (endTime - startTime))
-startTime = time.time()
+	fid = open(args.out+'vocabulary.txt', 'w')
+	fid.write(str(sorted(vocabulary)))
+	endTime = time.time()
+	print ("\tVocabulary saved\t\t\t%f seconds\n\n\tCounting n-grams..." % (endTime - startTime))
+	startTime = time.time()
 
-for sentence in sentences:
-	sentence = sentence.split()
+textFile = open(args.input)
+
+for line in textFile:
+	line = line.replace('\n', '')
+	
+	sentence = line.split()
 	ngrams = getNgram(sentence,N)
 	
 	for phrase in ngrams:
@@ -52,28 +56,19 @@ for sentence in sentences:
 			phrases[phrase] = phrases[phrase]+1
 		else:
 			phrases[phrase] = 1
-
-	# we also need the vocabulary
-	for word in sentence:
-		if word not in vocabulary:
-			vocabulary.append(word)
+textFile.close()
 
 endTime = time.time()
-print ("\tLearning complete\t\t\t%f seconds\n\n\tSaving model..." % (endTime - startTime))
+print ("\tCounting complete\t\t\t%f seconds\n\n\tSaving model..." % (endTime - startTime))
 startTime = time.time()
+
 total = 1.0*sum(phrases.values())
-fid = open('ngramModel.txt', 'w')
+fid = open(args.out+'ngramModel.txt', 'w')
 fid.write("%i Word Phrase" % (N)+" "*20+"Probability\n")
 fid.write('-'*91+'\n')
 for phrase in phrases:
 	for word in phrase:
   		fid.write("%s " % word)
 	fid.write("\t%.5e\n"%(phrases[phrase]/total))
-
 endTime = time.time()
-print ("\tModel saved\t\t\t\t%f seconds\n\n\tSaving vocabulary..." % (endTime - startTime))
-startTime = time.time()
-fid = open('vocabulary.txt', 'w')
-fid.write(str(sorted(vocabulary)))
-endTime = time.time()
-print ("\tVocabulary saved\t\t\t%f seconds\n" % (endTime - startTime))
+print ("\tModel saved\t\t\t%f seconds\n" % (endTime - startTime))
