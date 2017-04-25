8789032e52908c28c3f815305fb69e25625961dc
Roelof Groenewald <groenewa@usc.edu>
2017-04-25 15:51:47 -0700

    Update ngramRun.py
    Added ability to specify directory of vocabulary and model files. Also changed it so the recommended words are written to file which can be passed to the evaluation code

--- ngramRun.py
+++ ngramRun.py
@@ -1,5 +1,5 @@
 # Beibei, Jared and Roelof
-# Final Project - N-gram Training Algorithm
+# Final Project - N-gram Evaluation Algorithm
 # 04/06/2017 
 
 import sys
@@ -9,6 +9,14 @@ from collections import defaultdict
 import string
 import numpy as np
 import random
+
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("vocab", help="vocabulary file")
+parser.add_argument("model", help="model file")
+parser.add_argument("ts", help="test sentences file")
+args = parser.parse_args()
 
 startTime = time.time()
 
@@ -22,58 +30,41 @@ def getNgram(input_list, n):
 	## Read the input files
 	## ----------------------------------------------
 
-try:
-	print("\n\n\tN-gram Algorithm\n\n\tLoading vocabulary and \n\tmodel parameters...")
+#try:
+print("\n\n\tN-gram Algorithm\n\n\tLoading vocabulary and \n\tmodel parameters...")
 
-	vocabulary = eval(open('vocabulary.txt').read())
+vocabulary = eval(open(args.vocab).read())
+textFile = open(args.model)
 
-	textFile = open("ngramModel.txt")
+ii = 0
+for line in textFile:
+	if ii == 0:
+		N = int(line.split()[0])
+	if ii > 1:
+		line = line.split()
+		phrase = getNgram(line[0:N],N)[0]
+		phrases[phrase] = float(line[-1])
+	ii = ii+1
 
-	ii = 0
-	for line in textFile:
-		if ii == 0:
-			N = int(line.split()[0])
-		if ii > 1:
-			line = line.split()
-			phrase = getNgram(line[0:N],N)[0]
-			phrases[phrase] = float(line[-1])
-		ii = ii+1
-
-	textFile.close()
-except:
-	print ('\t'+'*'*56)
-	print ('\tError occurred during reading of training data... \n\tProgram terminated')
-	print ('\t'+'*'*56)
-	exit()
+textFile.close()
+#except:
+#	print ('\t'+'*'*56)
+#	print ('\tError occurred during reading of training data... \n\tProgram terminated')
+#	print ('\t'+'*'*56)
+#	exit()
 
 endTime = time.time()
-print ("\tLoading model data complete\t%f seconds\n\n\tReading input file..." % (endTime - startTime))
+print ("\tLoading model data complete\t%f seconds\n\n\tStarting analysis..." % (endTime - startTime))
 startTime = time.time()
 
-sentences =[]
-try:
-	textFile = open(sys.argv[1])
+textFile = open(args.ts)
+fid = open(str(N)+'-gram_answer.txt', 'w')
 	
-	for line in textFile:
-		line = line.translate(None, string.punctuation).lower().replace('\n', '')
-		sentences.append(line)
-	textFile.close()
-except:
-	print ('\t'+'*'*56)
-	print ('\tError occurred during reading of input data... \n\tProgram terminated')
-	print ('\t'+'*'*56)
-	exit()
-
-endTime = time.time()
-print ("\tLoading input data complete\t%f seconds\n\n\tStarting analysis..." % (endTime - startTime))
-startTime = time.time()
-
-success = 0
-
-for sentence in sentences:
-	sentence = sentence.split()
-	pos = random.randrange(0, len(sentence))
-	missingWord = sentence[pos]
+for line in textFile:
+	line = line.replace('\n', '')
+	sentence = line.split()
+	pos = sentence.index('/MISSING_WORD')
+	#missingWord = sentence[pos]
 	maxProb = 0.0
 	chosenWord = ''
 	for word in vocabulary:
@@ -88,11 +79,9 @@ for sentence in sentences:
 		if prob > maxProb:
 			maxProb = prob
 			chosenWord = word
-	#print chosenWord, missingWord
-	if (chosenWord == missingWord):
-		success = success + 1
+	fid.write(chosenWord+'\n')
+textFile.close()
+fid.close()
 
 endTime = time.time()
-print ("\tAnalysis complete\t\t%f seconds\n\n\tTotal sentences evaluated:\t%i\n\tSuccess rate:\t\t\t%.1f%%" % (endTime - startTime, len(sentences), (100.0*success/len(sentences))))
-
-
+print ("\tAnalysis complete\t\t%f seconds\n\n" % (endTime - startTime))
