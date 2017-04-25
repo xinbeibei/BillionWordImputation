5a395a904f83ee062f8e3a40a3330942cd84c5be
Jared Sagendorf <jsagendorf@gmail.com>
2017-04-25 11:43:35 -0700

    improved cleanCorpus further

--- cleanCorpus.py
+++ cleanCorpus.py
@@ -12,124 +12,122 @@ import numpy as np
 import argparse
 import random
 
+def hasNumbers(inputString):
+    if args.numbers:
+        return False
+    else:
+        return any(char.isdigit() for char in inputString)
+
+def onlyKnownWords(inputString):
+    for word in inputString:
+        if (word not in vocabulary) or (vocabulary[word] < N):
+            return False
+    return True
+
+# Method to remove a random word from
+# a sentence and replace it with the
+# missing word symbol: /MISSING_WORD
+def removeWord(sentence):
+    sentence[random.randint(1,len(sentence)-2)] = "/MISSING_WORD"
+    return ' '.join(sentence)
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("input", help="input file name")
-parser.add_argument("num", help="minimum number of occurences in full corpus", type=int)
-parser.add_argument("-smin", help="specify minimum length of sentence", type=int,default=2)
-parser.add_argument("-smax", help="specify maximum length of sentence", type=int,default=0)
-parser.add_argument("-split", help="specify percentage of corpus to seperate into test set", type=float,default=0.0)
-parser.add_argument("-num","--numbers", help="allow numbers in sentences", action="store_true")
-parser.add_argument("-hist", help="save histogram of sentence lengths", action="store_true")
-parser.add_argument("-afr", help="cleaning afrikaans corpus", action="store_true")
+parser.add_argument("num", help="minimum number of occurences for a word in the full corpus", type=int)
+parser.add_argument("--smin", help="specify minimum length of sentence", type=int, default=3)
+parser.add_argument("--smax", help="specify maximum length of sentence", type=int, default=9999)
+parser.add_argument("--split", help="specify percentage of corpus to seperate into test set", type=int, default=5)
+parser.add_argument("--allow_numbers", help="allow numbers in sentences", action="store_true", dest="numbers")
+parser.add_argument("--hist", help="save histogram of sentence lengths", action="store_true")
+parser.add_argument("--afr", help="cleaning afrikaans corpus", action="store_true")
 args = parser.parse_args()
 
 startTime = time.time()
 
 vocabulary = {} # this is the full vocabulary of words
 N = args.num
-if args.hist:
-	lengthHist = np.zeros(1000)
-
-	## ----------------------------------------------
-	## Read the input files
-	## ----------------------------------------------
+## ----------------------------------------------
+## Read the input files
+## ----------------------------------------------
 print("\n\n\tCleaning corpus to only include\n\tsentences in which each word\n\tappears at least "+str(N)+" times\n\n\tLoading corpus data...")
-	
+
+maxLen = 0
 textFile = open(args.input)
-	
 for line in textFile:
-	line = line.lower().replace('\n', '')
-	if args.afr:
-		if re.match("^[^<>]+$",line) is None:
-			continue
-		line = re.sub(ur"^[a-mo-z]\s",'',line)
-	line = re.sub(ur"[^\w\d'\s]+",'',line)
-	sentence = line.split()
-	# we also need the vocabulary
-	for word in sentence:
-		if word not in vocabulary:
-			#print word
-			vocabulary[word] = 1
-		else:
-			vocabulary[word] = vocabulary[word] + 1
+    line = line.lower().replace('\n', '')
+    if args.afr:
+        if re.match("^[^<>]+$",line) is None:
+            continue
+        line = re.sub(ur"^[a-mo-z]\s",'',line)
+    line = re.sub(ur"[^\w\d'\s]+",'',line)
+    sentence = line.split()
+    # Don't add words to vocab from sentences which won't be included
+    if(len(sentence) < args.smin):
+        continue
+    if(len(sentence) > args.smax):
+        continue
+    if(hasNumbers(sentence)):
+        continue
+    # we also need the vocabulary
+    for word in sentence:
+        if word not in vocabulary:
+            #print word
+            vocabulary[word] = 1
+        else:
+            vocabulary[word] = vocabulary[word] + 1
+    maxLen = max(maxLen, len(sentence))
 
 textFile.close()
 endTime = time.time()
 print ("\tLoading corpus data complete\t\t%f seconds\n\n\tCleaning started..." % (endTime - startTime))
 startTime = time.time()
 
-def hasNumbers(inputString):
-	if args.numbers:
-		return False
-	else:
-		return any(char.isdigit() for char in inputString)
-
-def onlyKnownWords(inputString):
-	for word in inputString:
-		if (word not in vocabulary) or (vocabulary[word] < N):
-			return False
-	return True
 
 textFile = open(args.input)
-cleanSentences = []
+tr_tar = open("clean_training_target.txt", 'w')
+tr_src = open("clean_training_source.txt", 'w')
+te_tar = open("clean_testing_target.txt", 'w')
+te_src = open("clean_testing_source.txt", 'w')
+count = 0
+rejected = 0
+if args.hist:
+    lengthHist = np.zeros(maxLen+1)
 for line in textFile:
-	line = line.lower().replace('\n', '')
-	if args.afr:
-		if re.match("^[^<>]+$",line) is None:
-			continue
-		line = re.sub(ur"^[a-mo-z]\s",'',line)
-	line = re.sub(ur"[^\w\d'\s]+",'',line)
-	sentence = line.split()
-	if args.hist:
-		try:
-			lengthHist[len(sentence)] = lengthHist[len(sentence)] + 1
-		except:
-			continue
-	
-	if len(sentence) < args.smin:
-		continue 
-	if args.smax > 0:
-		if len(sentence) > args.smax:
-			continue 
+    line = line.lower().replace('\n', '')
+    if args.afr:
+        if re.match("^[^<>]+$",line) is None:
+            continue
+        line = re.sub(ur"^[a-mo-z]\s",'',line)
+    line = re.sub(ur"[^\w\d'\s]+",'',line)
+    sentence = line.split()
+    
+    if len(sentence) < args.smin:
+        rejected += 1
+        continue 
+    if len(sentence) > args.smax:
+        rejected += 1
+        continue 
 
-	if (onlyKnownWords(sentence)) and not hasNumbers(sentence):
-		cleanSentences.append(line)
-
+    if (onlyKnownWords(sentence)) and not hasNumbers(sentence):
+        if( (args.split*count) % 100 == 0):
+            te_tar.write(' '.join(sentence)+"\n")
+            te_src.write(removeWord(sentence)+"\n")
+        else:
+            tr_tar.write(' '.join(sentence)+"\n")
+            tr_src.write(removeWord(sentence)+"\n")
+        count += 1
+        if args.hist:
+            lengthHist[len(sentence)] = lengthHist[len(sentence)] + 1
+    else:
+        rejected += 1
 textFile.close()
-endTime = time.time()
-print ("\tCleaning complete\t\t\t%f seconds\n\n\tSaving corpus..." % (endTime - startTime))
-startTime = time.time()
-
-# Method to remove a random word from
-# a sentence and replace it with the
-# missing word symbol: /MISSING_WORD
-def removeWord(inputString):
-	sentence = inputString.split()
-	sentence[random.randint(1,len(sentence)-1)] = "/MISSING_WORD"
-	return ' '.join(sentence)
-
-cut = int(len(cleanSentences) * (100 - args.split)/100)
-fid = open("clean_training_target.txt", 'w')
-fid2 = open("clean_training_source.txt", 'w')
-for ii in range(0, cut):
-	fid.write(cleanSentences[ii]+"\n")
-	fid2.write(removeWord(cleanSentences[ii])+"\n")
-fid.close()
-fid2.close()
-if args.split > 0.0:
-	fid = open("clean_test_target.txt", 'w')
-	fid2 = open("clean_test_source.txt", 'w')
-	for ii in range(cut, len(cleanSentences)):
-		fid.write(cleanSentences[ii]+"\n")
-		fid2.write(removeWord(cleanSentences[ii])+"\n")
-	fid.close()
-	fid2.close()
 
 if args.hist:
-	fid = open('histogram.dat', 'w')
-	for ii in range(0,500):
-		fid.write(str(ii)+'\t'+str(lengthHist[ii])+"\n")
+    fid = open('histogram.dat', 'w')
+    for ii in range(len(lengthHist)):
+        fid.write(str(ii)+'\t'+str(lengthHist[ii])+"\n")
 
 endTime = time.time()
 print ("\tCorpus saved\t\t\t\t%f seconds\n" % (endTime - startTime))
+print ("\tKept %d sentences and rejected %d." %(count, rejected))
